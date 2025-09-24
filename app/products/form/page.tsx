b90ea859e69b8cce0f2e02be0851fe3264ace07e
Ricky Andrian <rickyandrian1993@gmail.com>
2025-09-24 22:49:21 +0700

    Hold the add product function

--- app/products/form/page.tsx
+++ app/products/form/page.tsx
@@ -1,20 +1,18 @@
-import Link from 'next/link'
-import FormInput from './FormInput'
-import ProductLists from './ProductLists'
+import Link from "next/link";
 
 export default function ProductForm() {
   return (
     <>
       <Link
         className="w-full bg-[#ecebe8] rounded-xl shadow hover:shadow-lg p-2 flex flex-col items-center text-center transition-all duration-200 transform hover:scale-105 hover:cursor-pointer mt-4"
-        href={'/products'}
+        href={"/products"}
       >
         Back to Products
       </Link>
       {/* Add New Product Form */}
-      <FormInput />
+      {/* <FormInput />
 
-      <ProductLists />
+      <ProductLists /> */}
     </>
-  )
+  );
 }
