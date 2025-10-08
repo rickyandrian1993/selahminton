fb31374a18af4119d3d15ab31b96290d3c6f6277
Ricky Andrian <ricky.andrian@primecash.co.id>
2025-10-08 12:59:07 +0700

    Adding text join us

--- components/Menus.tsx
+++ components/Menus.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Menus() {
   return (
@@ -8,7 +8,7 @@ export default function Menus() {
       <button
         className="w-full bg-[#ecebe8] rounded-xl shadow hover:shadow-lg p-2 flex flex-col items-center text-center transition-all duration-200 transform hover:scale-105 hover:cursor-pointer"
         onClick={() =>
-          window.open("https://www.instagram.com/selahminton", "_blank")
+          window.open('https://www.instagram.com/selahminton', '_blank')
         }
       >
         Instagram
@@ -16,7 +16,7 @@ export default function Menus() {
       <button
         className="w-full bg-[#ecebe8] rounded-xl shadow hover:shadow-lg p-2 flex flex-col items-center text-center transition-all duration-200 transform hover:scale-105 hover:cursor-pointer"
         onClick={() =>
-          window.open("https://www.tiktok.com/@selahminton", "_blank")
+          window.open('https://www.tiktok.com/@selahminton', '_blank')
         }
       >
         Tiktok
@@ -24,7 +24,7 @@ export default function Menus() {
       <button
         className="w-full bg-[#ecebe8] rounded-xl shadow hover:shadow-lg p-2 flex flex-col items-center text-center transition-all duration-200 transform hover:scale-105 hover:cursor-pointer"
         onClick={() =>
-          window.open("https://www.youtube.com/@selahminton-25", "_blank")
+          window.open('https://www.youtube.com/@selahminton-25', '_blank')
         }
       >
         Youtube
@@ -33,19 +33,19 @@ export default function Menus() {
         className="w-full bg-[#ecebe8] rounded-xl shadow hover:shadow-lg p-2 flex flex-col items-center text-center transition-all duration-200 transform hover:scale-105 hover:cursor-pointer"
         onClick={() =>
           window.open(
-            "https://link.ayo.co.id/l/community-26674_ref_RICGHX",
-            "_blank"
+            'https://link.ayo.co.id/l/community-26674_ref_RICGHX',
+            '_blank'
           )
         }
       >
-        Ayo Community
+        Ayo Community (Join Us)
       </button>
       <Link
         className="w-full bg-[#ecebe8] rounded-xl shadow hover:shadow-lg p-2 flex flex-col items-center text-center transition-all duration-200 transform hover:scale-105 hover:cursor-pointer"
-        href={"/products"}
+        href={'/products'}
       >
         Badminton Equipments
       </Link>
     </div>
-  );
+  )
 }
